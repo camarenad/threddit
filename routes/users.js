dce25a4c0222329e2db31a22a93816085ee9fead
Daniel Camarena <dcamarena0229@gmail.com>
2019-05-30 11:48:37 -0700

    authenticate routes

--- routes/users.js
+++ routes/users.js
@@ -4,14 +4,14 @@ var userCtrl = require('../controllers/users')
 
 /* GET users listing. */
 router.get('/',userCtrl.index)
-router.get('/profile/:id', userCtrl.profile)
-router.post('/post', userCtrl.addPost)
+router.get('/profile/:id', userCtrl.profile,isLoggedIn)
+router.post('/post', userCtrl.addPost,isLoggedIn)
 router.get('/post',userCtrl.post,isLoggedIn)
-router.delete('/profile/:id', userCtrl.delPost )
-router.get('/edit/:id',userCtrl.show)
-router.put('/edit/:id',userCtrl.updatePost)
+router.delete('/profile/:id', userCtrl.delPost,isLoggedIn)
+router.get('/edit/:id',userCtrl.show,isLoggedIn)
+router.put('/edit/:id',userCtrl.updatePost,isLoggedIn)
 router.get('/post/:id', userCtrl.showComments,isLoggedIn)
-router.post('/comment/:id',userCtrl.newComment)
+router.post('/comment/:id',userCtrl.newComment,isLoggedIn)
 
 function isLoggedIn(req, res, next) {
     if ( req.isAuthenticated() ) return next();
